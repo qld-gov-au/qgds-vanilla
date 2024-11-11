bb202ef56f2265616f8eee96847a16a692e8cc8e
Pedro Portella <contact@pedroportella.com>
2024-11-12 09:39:56 +1000

    feat: pagination

--- src/stories/templates/landing/landing.stories.js
+++ src/stories/templates/landing/landing.stories.js
@@ -15,10 +15,12 @@ import header from "./landing-header.json";
 import navigation from "./landing-navigation.json";
 import mega_main_navigation from "./landing-mega_main_navigation.json";
 import breadcrumbs from "./landing-breadcrumbs.json";
-import abstract from "./landing-abstract.json";
+import card_multi_action from "./landing-card_multi_action.json";
 import accordion from "./landing-accordion.json";
 import callout from "./landing-callout.json";
 import button from "./landing-button.json";
+import horizontal_rule from "./landing-horizontal_rule.json";
+import pagination from "./landing-pagination.json";
 import internal_navigation from "./landing-internal_navigation.json";
 import widgets from "./landing-widgets.json";
 import banner_intermediate from "./landing-banner_intermediate.json";
@@ -53,10 +55,12 @@ let json_data = {
     internal_navigation: internal_navigation,
     banner_intermediate: banner_intermediate,
     content: content,
-    abstract: abstract,
+    card_multi_action: card_multi_action,
     accordion: accordion,
     callout: callout,
     button: button,
+    horizontal_rule: horizontal_rule,
+    pagination: pagination,
     widgets: widgets,
     footer: footer,
     footer_scripts: footer_scripts
