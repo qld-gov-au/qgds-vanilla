eed772529c4d93fccb47cdade774447d507769e2
Steve Pegg <126438309+stvp-qld@users.noreply.github.com>
2024-12-20 12:04:33 +1100

    Feature: Convert Back-to-top widget to a component to match other patterns

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,66 +1,65 @@
-import { mergeConfig } from 'vite';
-import customViteConfig from '../vite.config.js'; // Adjust the path as needed
+import { mergeConfig } from "vite";
+import customViteConfig from "../vite.config.js"; // Adjust the path as needed
 
 /** @type { import('@storybook/html-vite').StorybookConfig } */
 const config = {
-    core: {
-        disableTelemetry: true,
-    },
-    stories: [
-        //"../src/stories/Introduction.mdx",
-        // Include all stories found under the src/components directory ( For example: alert/alert.stories.js )
-        // Exlude any stories starting with an underscore ( For example: _exludeme.stories.js )
-        "../src/**/!(*_)*.mdx",
-        "../src/**/!(*_)*.stories.js",
-    ],
-    staticDirs: [
-        //{ from: '../src/', to: '/' },
-        { from: '../dist/assets/css', to: '/assets/css' },
-        { from: '../dist/assets/js', to: '/assets/js' },
-        { from: '../dist/assets/helpers', to: '/assets/helpers' },
-        { from: '../src/img', to: '/assets/img' },
-    ], //Bring dist in statically instead of having it minified
-    addons: [//Storybook addons
+  core: {
+    disableTelemetry: true,
+  },
+  stories: [
+    //"../src/stories/Introduction.mdx",
+    // Include all stories found under the src/components directory ( For example: alert/alert.stories.js )
+    // Exlude any stories starting with an underscore ( For example: _exludeme.stories.js )
+    "../src/**/!(*_)*.mdx",
+    "../src/**/!(*_)*.stories.js",
+  ],
+  staticDirs: [
+    //{ from: '../src/', to: '/' },
+    { from: "../dist/assets/css", to: "/assets/css" },
+    { from: "../dist/assets/js", to: "/assets/js" },
+    { from: "../dist/assets/helpers", to: "/assets/helpers" },
+    { from: "../src/img", to: "/assets/img" },
+  ], //Bring dist in statically instead of having it minified
+  addons: [
+    //Storybook addons
     //https://storybook.js.org/addons/
-        "@storybook/addon-a11y",
-        "@storybook/addon-themes",
-        "@storybook/addon-essentials",
-        "@storybook/addon-interactions",
-        {
-            name: "@storybook/addon-storysource",
-            options: {
-                loaderOptions: {
-                    injectStoryParameters: true,
-                },
-            },
+    "@storybook/addon-a11y",
+    "@storybook/addon-themes",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-storysource",
+      options: {
+        loaderOptions: {
+          injectStoryParameters: true,
         },
-        "@storybook/addon-links",
-        "@chromatic-com/storybook",
-        // "@storybook/addon-designs" //re-introduce once its not causing peerDependancy errors
-    ],
-
-    framework: {
-        //Build the storybook with html-vite rendered - faster than webpack
-        //https://www.npmjs.com/package/@storybook/html-vite
-        name: "@storybook/html-vite",
-        options: {},
+      },
     },
+    "@storybook/addon-links",
+    "@chromatic-com/storybook",
+    // "@storybook/addon-designs" //re-introduce once its not causing peerDependancy errors
+  ],
 
-    //Autodocs for each component
-    //https://storybook.js.org/docs/writing-docs/autodocs
-    docs: {
-        defaultName: 'Overview'
-    },
+  framework: {
+    //Build the storybook with html-vite rendered - faster than webpack
+    //https://www.npmjs.com/package/@storybook/html-vite
+    name: "@storybook/html-vite",
+    options: {},
+  },
 
+  //Autodocs for each component
+  //https://storybook.js.org/docs/writing-docs/autodocs
+  docs: {
+    defaultName: "Overview",
+  },
 
-
-    // https://storybook.js.org/docs/api/main-config-vite-final
-    // Use the Vite configuration from the main project (yes this is a esbuild project but storybook uses vite)
-    async viteFinal(config) {
-        // Merge custom Vite configuration
-        //console.log(JSON.stringify(mergeConfig(config, customViteConfig.default)));
-        return mergeConfig(config, customViteConfig.default);
-    },
+  // https://storybook.js.org/docs/api/main-config-vite-final
+  // Use the Vite configuration from the main project (yes this is a esbuild project but storybook uses vite)
+  async viteFinal(config) {
+    // Merge custom Vite configuration
+    //console.log(JSON.stringify(mergeConfig(config, customViteConfig.default)));
+    return mergeConfig(config, customViteConfig.default);
+  },
 };
 
 export default config;
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,8 @@
 import Example from "../components/_example/index.js";
-// import Button from "../components/button/index.js";
+import BackToTop from "../components/back_to_top/index.js";
 // import Dropdown from "../components/dcropdown/index.js";
 
 export const QGDS = {
   Example,
+  BackToTop,
 };
