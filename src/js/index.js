986cf14736f5e69c06722542fd017e692ae2f045
Sen Chung <SenChung@users.noreply.github.com>
2025-01-07 16:54:37 +1000

    Callout and quotes more

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,10 @@
 import Example from "../components/_example/index.js";
 import BackToTop from "../components/back_to_top/index.js";
+import Callout from "../components/callout/index.js";
 // import Dropdown from "../components/dcropdown/index.js";
 
 export const QGDS = {
   Example,
   BackToTop,
+  Callout,
 };
